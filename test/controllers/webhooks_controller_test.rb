ff6b0423c3612aa35c90ffe9d30b3b72970cc569
Michael Hale <mhale@heroku.com>
2017-06-06 12:10:26 -0600

    add webhooks controller integration test

--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can receive and store a webhook" do
+    payload = { "foo" => "bar" }
+    post webhooks_url, as: :json, params: payload
+    assert_response :success
+    assert_equal payload, Event.last.payload
+  end
 end
