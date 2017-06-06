6de10f71d4f00062c7a4f0dc4cc5612ce9f0fcbd
Ransom Briggs <rbriggs@heroku.com>
2017-06-06 15:28:58 -0600

    Dump out the webhook signing header

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,5 +1,9 @@
 class WebhooksController < ApplicationController
   def create
+    digest  = OpenSSL::Digest.new('sha256')
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['WEBHOOK_SECRET'], request.raw_post)).strip
+    logger.info(calculated_hmac)
+    logger.info(request.headers['Heroku-Webhook-Hmac-SHA256'])
     Event.create(payload: params['webhook'])
     # TODO ensure transaction around request?
     # TODO: trim events list?
