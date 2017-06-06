d3378de69dca3cea635306e7aa9cc6666e5bbbf4
Ransom Briggs <rbriggs@heroku.com>
2017-06-06 15:43:56 -0600

    Verify that the signatures match

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -2,10 +2,14 @@ class WebhooksController < ApplicationController
   def create
     digest  = OpenSSL::Digest.new('sha256')
     calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['WEBHOOK_SECRET'], request.raw_post)).strip
-    logger.info(calculated_hmac)
-    logger.info(request.headers['Heroku-Webhook-Hmac-SHA256'])
-    Event.create(payload: params['webhook'])
-    # TODO ensure transaction around request?
-    # TODO: trim events list?
+    heroku_hmac = request.headers['Heroku-Webhook-Hmac-SHA256']
+
+    if Rack::Utils.secure_compare(calculated_hmac, heroku_hmac)
+      Event.create(payload: params['webhook'])
+      # TODO ensure transaction around request?
+      # TODO: trim events list?
+    else
+      render json: {'error' => 'signature_mismatch'}, status: 403
+    end
   end
 end
