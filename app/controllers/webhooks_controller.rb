54eb5f3796a79c74351cb7b5f31bd0c01735ed1e
Michael Hale <mhale@heroku.com>
2017-06-12 17:57:20 -0400

    test signature, refactor controller

--- app/controllers/webhooks_controller.rb
+++ app/controllers/webhooks_controller.rb
@@ -1,10 +1,6 @@
 class WebhooksController < ActionController::API
   def create
-    digest  = OpenSSL::Digest.new('sha256')
-    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(digest, ENV['WEBHOOK_SECRET'], request.raw_post)).strip
-    heroku_hmac = request.headers['Heroku-Webhook-Hmac-SHA256']
-
-    if Rack::Utils.secure_compare(calculated_hmac, heroku_hmac)
+    if valid_signature?
       event = Event.create(payload: params['webhook'])
       serialized_event = ActiveModelSerializers::SerializableResource.new(event, {serializer: EventSerializer})
       ActionCable.server.broadcast 'events', serialized_event
@@ -14,5 +10,17 @@ class WebhooksController < ActionController::API
     else
       render json: {'error' => 'signature_mismatch'}, status: 403
     end
+  end
+
+  private
+  def valid_signature?
+    calculated_hmac = Base64.encode64(OpenSSL::HMAC.digest(
+      OpenSSL::Digest.new('sha256'),
+      ENV['WEBHOOK_SECRET'],
+      request.raw_post
+    )).strip
+    heroku_hmac = request.headers['Heroku-Webhook-Hmac-SHA256']
+
+    heroku_hmac && Rack::Utils.secure_compare(calculated_hmac, heroku_hmac)
   end
 end
--- test/controllers/webhooks_controller_test.rb
+++ test/controllers/webhooks_controller_test.rb
@@ -1,10 +1,33 @@
 require 'test_helper'
 
 class WebhooksControllerTest < ActionDispatch::IntegrationTest
+  test "it requires a signature" do
+    post webhooks_url, as: :json, params: {}
+    assert_response :forbidden
+  end
+
+  test "it does not accept invalid requests" do
+    payload = { "foo" => "bar" }
+    post webhooks_url, as: :json, params: payload, headers: signature_header(payload, "boom!")
+    assert_response :forbidden
+  end
+
   test "can receive and store a webhook" do
     payload = { "foo" => "bar" }
-    post webhooks_url, as: :json, params: payload
+    post webhooks_url, as: :json, params: payload, headers: signature_header(payload)
     assert_response :success
     assert_equal payload, Event.last.payload
+  end
+
+  def signature_header(payload, secret = ENV['WEBHOOK_SECRET'])
+    signature = Base64.encode64(OpenSSL::HMAC.digest(
+      OpenSSL::Digest.new('sha256'),
+      secret,
+      payload.to_json)
+    ).strip
+
+    {
+      'Heroku-Webhook-Hmac-SHA256' => signature
+    }
   end
 end
