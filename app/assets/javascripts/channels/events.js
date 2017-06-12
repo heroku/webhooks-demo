e3d9a3757d7d75402d8dad498393e3c07b613b14
Matt Gauger <matt.gauger@gmail.com>
2017-06-12 16:28:06 -0500

    Cleanup

--- app/assets/javascripts/channels/events.js
+++ app/assets/javascripts/channels/events.js
@@ -1,14 +1,6 @@
 App.events = App.cable.subscriptions.create('EventsChannel', {
     received: function(data) {
-        //var p = document.createElement("pre");
         console.log(data);
-        //p.innerHTML = JSON.stringify(data);
-        //document.body.appendChild(p);
-
-    //<div class="events-list-row dt-row hover-bg-lightest-silver">
-    //<div class="fl w-third pv2 mv1 pl2 f4"><%= event.created_at %></div>
-    //<pre class="events-payload ba b--silver br2 code lh-copy w-100 w-60-l h4 overflow-x-scroll overflow-y-scroll ph1 pv2 mv1 fl f4"><%= event.payload %></pre>
-    //</div>
 
         var list = $('.events-list');
 
