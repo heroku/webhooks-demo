aa14733fcad747320a9bf67325dcb2a8969c7cc8
Ransom Briggs <rbriggs@salesforce.com>
2021-07-06 13:14:18 -0500

    bundle update rails & precompile

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( tabs.js dashboard.js )
+Rails.application.config.assets.precompile += %w( tabs.js dashboard.js application.css application.js )
