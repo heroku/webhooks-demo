88e1ff4828f10da9484ae84d91bed19cd03c59d5
Marcel Santos <marcelsantos@salesforce.com>
2024-03-26 13:32:02 -0600

    remove authentication

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,5 +1,5 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user_action!
+  #before_action :authenticate_user_action!
 
   def index
     # render json: Event.order(created_at: :desc).limit(100), each_serializer: EventSerializer
